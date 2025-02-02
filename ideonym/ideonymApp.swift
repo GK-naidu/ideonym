8a8fca91bd41f2ce04eb177019e8827b3519ed30
G.K.Naidu <plurals.reboots-0y@icloud.com>
2025-02-02 12:22:29 +0530

    Initial Commit

---
+++ ideonym/ideonymApp.swift
@@ -0,0 +1,17 @@
+//
+//  ideonymApp.swift
+//  ideonym
+//
+//  Created by GK Naidu on 02/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ideonymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
