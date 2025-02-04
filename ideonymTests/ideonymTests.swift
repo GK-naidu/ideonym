8ecf71df54a3a6ac56be6e6f41e5af82ba6d3582
G.K.Naidu <plurals.reboots-0y@icloud.com>
2025-02-04 16:56:37 +0530

    removed test files

--- ideonymTests/ideonymTests.swift
+++ ideonymTests/ideonymTests.swift
@@ -1,17 +1,1 @@
-//
-//  ideonymTests.swift
-//  ideonymTests
-//
 //  Created by GK Naidu on 02/02/25.
-//
-
-import Testing
-@testable import ideonym
-
-struct ideonymTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
-}
